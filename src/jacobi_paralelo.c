b5826a9df25c9729dbd1a8bc4381b8fcea33f2b3
Breno <breno.bernal@unesp.br>
2025-09-19 21:58:58 -0300

    Bernal voltou

--- src/jacobi_paralelo.c
+++ src/jacobi_paralelo.c
@@ -10,47 +10,41 @@
 void ler_entrada(double **A, double *b, const char *filename)
 {
     FILE *file = fopen(filename, "r");
-    if (file == NULL)
+    if (!file)
     {
         perror("Erro ao abrir o arquivo de entrada");
         exit(EXIT_FAILURE);
     }
     for (int i = 0; i < N; i++)
-    {
         for (int j = 0; j < N; j++)
-        {
             if (fscanf(file, "%lf", &A[i][j]) != 1)
             {
                 fprintf(stderr, "Erro ao ler a matriz A na linha %d, coluna %d\n", i + 1, j + 1);
                 fclose(file);
                 exit(EXIT_FAILURE);
             }
-        }
-    }
+
     for (int i = 0; i < N; i++)
-    {
         if (fscanf(file, "%lf", &b[i]) != 1)
         {
             fprintf(stderr, "Erro ao ler o vetor b na coluna %d\n", i + 1);
             fclose(file);
             exit(EXIT_FAILURE);
         }
-    }
+
     fclose(file);
 }
 
 void escrever_saida(double *x, const char *filename)
 {
     FILE *file = fopen(filename, "w");
-    if (file == NULL)
+    if (!file)
     {
         perror("Erro ao criar o arquivo de saída");
         exit(EXIT_FAILURE);
     }
     for (int i = 0; i < N; i++)
-    {
         fprintf(file, "%.4f ", x[i]);
-    }
     fprintf(file, "\n");
     fclose(file);
 }
@@ -58,7 +52,7 @@ void escrever_saida(double *x, const char *filename)
 void jacobi_paralelo(double **A, double *b, double *x, int threads)
 {
     double *x_novo = (double *)malloc(N * sizeof(double));
-    if (x_novo == NULL)
+    if (!x_novo)
     {
         perror("Erro ao alocar memória para x_novo");
         exit(EXIT_FAILURE);
@@ -66,9 +60,8 @@ void jacobi_paralelo(double **A, double *b, double *x, int threads)
     int iteracoes = 0;
     double erro;
     for (int i = 0; i < N; i++)
-    {
         x[i] = 0.0;
-    }
+
     omp_set_num_threads(threads);
     do
     {
@@ -84,26 +77,24 @@ void jacobi_paralelo(double **A, double *b, double *x, int threads)
             }
             double soma = 0.0;
             for (int j = 0; j < N; j++)
-            {
                 if (i != j)
-                {
                     soma += A[i][j] * x[j];
-                }
-            }
+
             x_novo[i] = (b[i] - soma) / A[i][i];
         }
+
 #pragma omp parallel for reduction(max : erro)
         for (int i = 0; i < N; i++)
         {
             double diff = fabs(x_novo[i] - x[i]);
             if (diff > erro)
-            {
                 erro = diff;
-            }
             x[i] = x_novo[i];
         }
+
         iteracoes++;
     } while (erro > 1e-5);
+
     printf("Método paralelo de Jacobi convergiu em %d iterações.\n", iteracoes);
     free(x_novo);
 }
@@ -120,13 +111,8 @@ int main(int argc, char *argv[])
     const char *schedule_type = (argc > 4) ? argv[4] : "static";
     int is_valid = 0;
     for (int i = 0; i < 3; i++)
-    {
         if (strcmp(schedule_type, valid_schedules[i]) == 0)
-        {
             is_valid = 1;
-            break;
-        }
-    }
 
     if (!is_valid)
     {
@@ -135,46 +121,11 @@ int main(int argc, char *argv[])
     }
 
     double **A = (double **)malloc(N * sizeof(double *));
-    if (A == NULL)
-    {
-        perror("Erro ao alocar memória para a matriz A");
-        return EXIT_FAILURE;
-    }
-    for (int i = 0; i < N; i++)
-    {
-        A[i] = (double *)malloc(N * sizeof(double));
-        if (A[i] == NULL)
-        {
-            perror("Erro ao alocar memória para a linha da matriz A");
-            for (int j = 0; j < i; j++)
-            {
-                free(A[j]);
-            }
-            free(A);
-            return EXIT_FAILURE;
-        }
-    }
-
     double *b = (double *)malloc(N * sizeof(double));
-    if (b == NULL)
-    {
-        perror("Erro ao alocar memória para o vetor b");
-        for (int i = 0; i < N; i++)
-            free(A[i]);
-        free(A);
-        return EXIT_FAILURE;
-    }
-
     double *x = (double *)malloc(N * sizeof(double));
-    if (x == NULL)
-    {
-        perror("Erro ao alocar memória para o vetor x");
-        for (int i = 0; i < N; i++)
-            free(A[i]);
-        free(A);
-        free(b);
-        return EXIT_FAILURE;
-    }
+
+    for (int i = 0; i < N; i++)
+        A[i] = (double *)malloc(N * sizeof(double));
 
     int num_threads = atoi(argv[3]);
     int chunk_size = (argc > 5) ? atoi(argv[5]) : 0;
@@ -185,18 +136,26 @@ int main(int argc, char *argv[])
 
     ler_entrada(A, b, argv[1]);
 
-    double start_time = omp_get_wtime();
-    jacobi_paralelo(A, b, x, num_threads);
-    double end_time = omp_get_wtime();
+    double tempos[3];
+    double soma = 0.0;
 
-    printf("Tempo de execução com %d threads, schedule %s e chunk size %d: %f segundos\n", num_threads, schedule_type, chunk_size, end_time - start_time);
+    for (int k = 0; k < 3; k++)
+    {
+        double start_time = omp_get_wtime();
+        jacobi_paralelo(A, b, x, num_threads);
+        double end_time = omp_get_wtime();
+
+        tempos[k] = end_time - start_time;
+        soma += tempos[k];
+        printf("Execução %d: tempo = %.6f s\n", k + 1, tempos[k]);
+    }
+
+    printf("Tempo médio: %.6f s\n", soma / 3.0);
 
     escrever_saida(x, argv[2]);
 
     for (int i = 0; i < N; i++)
-    {
         free(A[i]);
-    }
     free(A);
     free(b);
     free(x);
